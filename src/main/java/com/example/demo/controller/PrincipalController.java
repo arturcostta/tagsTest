ec1577ef14ed598951a06f1fcdb1d625029b99e4
Artur Costa <arturcostta@gmail.com>
2020-07-09 15:19:03 -0300

    segundo commit

---
+++ src/main/java/com/example/demo/controller/PrincipalController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrincipalController {
+
+    @GetMapping("/saudacao")
+    public String ola(){
+        return "ola";
+    }
+}
