67ce25787a7a33659e804fec20ba0afc6b3cf22f
Artur Costa <arturcostta@gmail.com>
2020-07-09 15:54:34 -0300

    segundo metodo

--- src/main/java/com/example/demo/controller/PrincipalController.java
+++ src/main/java/com/example/demo/controller/PrincipalController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class PrincipalController {
     @GetMapping("/saudacao")
     public String ola(){
         return "ola usuário, tudo bem?";
+    }
+
+    @GetMapping("/saudacao/{nome}")
+    public String ola(@PathVariable String nome){
+        return "ola " + nome + " tudo bem?";
     }
 }
